7d955d2cded317aba5446df30dc7a3c51f10ff79
nelrsf <nsantamaria@tiresia.co>
2023-04-09 14:10:22 -0500

    added test endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
+require('dotenv').config();
 
 @Controller()
 export class AppController {
   constructor() { }
+
+  @Get('/getdatabaseurl')
+  findAll(@Res() res) {
+    res.status(HttpStatus.OK).json(process.env.DATABASE_URL);
+  }
 
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,6 @@ require('dotenv').config();
 export class AppService {
 
   getDbUrlConectionStringByDbName(dbName: string) {
-    console.log(process.env.DATABASE_URL)
-    console.log(process.env.DATABASE_URL.replace('DATABASE_NAME', dbName))
     return process.env.DATABASE_URL.replace('DATABASE_NAME', dbName);
   }
 }
