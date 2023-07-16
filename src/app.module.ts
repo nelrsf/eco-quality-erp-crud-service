2046b099cb0661226663b88dbf76cf91f2ca97bb
nelrsf <nsantamaria@tiresia.co>
2023-07-15 22:36:21 -0500

    fix guards imports 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,9 @@ import { TablesModule } from './eqmodules/tables/tables.module';
 import { RowsModule } from './eqmodules/rows/rows.module';
 import { ColumnsModule } from './eqmodules/columns/columns.module';
 import { FilterUserModules } from './middlewares/FilterUserDatabases';
+import { ModuleAdminGuard } from './guards/module-admin.guard';
+import { TableAdminGuard } from './guards/table-admin.guard';
+import { ColumnAdminGuard } from './guards/column-admin.guard';
 
 
 @Module({
@@ -20,7 +23,7 @@ import { FilterUserModules } from './middlewares/FilterUserDatabases';
     ColumnsModule
   ],
   controllers: [AppController],
-  providers: [AppService, ErrorDataHandler, Connection]
+  providers: [AppService, ErrorDataHandler, Connection, ModuleAdminGuard, TableAdminGuard, ColumnAdminGuard]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
